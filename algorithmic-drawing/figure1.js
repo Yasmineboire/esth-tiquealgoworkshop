b325497dc32124e1b99f3bde2e7d4df31aa7ef78
Yasmine BOIRE <yasmine.boire@gmail.com>
2026-01-12 15:26:15 +0100

    Update figure1.js

--- algorithmic-drawing/figure1.js
+++ algorithmic-drawing/figure1.js
@@ -1,24 +1,25 @@
-let Xrsl = 640;
-let Yrsl = 480;
+let Xrsl = 640;//initialisation abscisses
+let Yrsl = 480;//initialisation ordonées
 
 function setup() {
-  createCanvas(Xrsl, Yrsl);
-  background(255);
-  noFill();
-  stroke(0);
+  createCanvas(Xrsl, Yrsl);//initialisation du rectangle
+  background(255);//fond blanc
+  noFill();//formes non remplies
+  stroke(0);//couleur noir 
 
-  let N = 0;
-  let X = Xrsl;
-  let Y = Yrsl;
-  let D = 0;
+  let N = 0;//position du coin supérieur gauche du rectangle
+  let X = Xrsl;//abcisse
+  let Y = Yrsl;//ordonée
+  let D = 0;//épaisseur
 
-  while (X - N > 0 && Y - N > 0) {
-    D++;
+  while (X - N > 0 && Y - N > 0)//empêche de dessiner des rectangles trop petits
+  {
+    D++;//augmentation de l'épaisseur
     strokeWeight(D);
 
     N = N + D + 1;
-    X = X - D - 10;
-    Y = Y - D - 10;
+    X = X - D - 10;//réduction de la largeur
+    Y = Y - D - 10;//réduction de la haureur 
 
     rect(N, N, X - N, Y - N);
   }
